d9a072a9711b836e40c9016c2232544b7087b876
Arun Mittal <arunmittal53@gmail.com>
2019-05-04 18:58:53 +0530

    Spring boot project of topics and courses taught in institute

---
+++ src/main/java/io/arunjava/courseApi/course/CourseRepository.java
@@ -0,0 +1,11 @@
+package io.arunjava.courseApi.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String>{
+	
+	public List<Course> findByTopicId(String topicId);
+
+}
